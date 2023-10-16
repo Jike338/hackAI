6b73f8081656c3d731d4f11c4618f0d9a0fe4493
Jike Zhong <jike_338@Jikes-MacBook-Pro.local>
2023-10-16 14:19:03 -0700

    chagne:ewq

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,8 +10,17 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     prompt = request.json['prompt']
-    result = f"Processed: {prompt}"  # Replace this line with your desired processing
-    print("result")
+
+    # Execute the curl command
+    try:
+        output = subprocess.check_output(["curl", "aaa.com"], text=True)
+        # if you want the raw bytes, remove the text=True argument
+    except subprocess.CalledProcessError as e:
+        output = f"Error executing curl: {e}"
+
+    print(f"CURL Output: {output}")
+
+    result = f"Processed: {prompt}"  # You can integrate the output into this result if needed
     return jsonify({"result": result})
 
 if __name__ == '__main__':
