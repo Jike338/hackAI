cd9caa2d9ff9a2bc320778b9b390a8d0256763db
Jike Zhong <jike_338@Jikes-MacBook-Pro.local>
2023-10-16 13:59:11 -0700

    change

--- static/index.js
+++ static/index.js
@@ -1,5 +1,15 @@
+document.getElementById("userInput").addEventListener("keypress", function(event) {
+    if (event.keyCode == 13) { // Check if "Enter" was pressed
+        event.preventDefault(); // Prevent the default action (like form submission)
+        processInput();
+    }
+});
+
 function processInput() {
     const userInput = document.getElementById("userInput").value;
+    appendMessage(userInput, 'user');
+    document.getElementById("userInput").value = "";
+
     fetch('/process', {
         method: 'POST',
         headers: {
@@ -9,9 +19,27 @@ function processInput() {
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById("result").textContent = data.result;
+        setTimeout(() => { // Delay the bot's response
+            appendMessage(data.result, 'bot');
+        }, 1000); // Delay by 1 second
     })
     .catch(error => {
         console.error('Error:', error);
     });
+}
+
+function appendMessage(message, sender) {
+    const chatbox = document.getElementById("chatbox");
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message", sender);
+    messageDiv.textContent = message;
+
+    // Append the message and make it visible with a delay
+    chatbox.appendChild(messageDiv);
+    setTimeout(() => {
+        messageDiv.style.opacity = "1";
+    }, 50);
+
+    // Scroll to the newest message
+    chatbox.scrollTop = chatbox.scrollHeight;
 }
